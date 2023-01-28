44a5eaaeafc91ff9695bf7419aee4ac8aff5f94d
iPig <49023948+iPigTW@users.noreply.github.com>
2023-01-28 19:25:43 +0800

    Update pighub.lua

--- pighub.lua
+++ pighub.lua
@@ -1,3 +1,4 @@
+getgenv().SecureMode = true 
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 local Window = Rayfield:CreateWindow({
     Name = "Pig Hub",
